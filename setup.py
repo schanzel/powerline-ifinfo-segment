85a155f14abaebe8923bb861efc31db7c225e742
Benjamin Schanzel <b.schanzel@gmail.com>
2020-10-07 19:20:33 +0200

    Set download_url in setup.py

--- setup.py
+++ setup.py
@@ -8,6 +8,7 @@ setup(
     author_email="b.schanzel@gmail.com",
     packages=["powerline_ifinfo"],
     url="https://github.com/schanzel/powerline-ifinfo-segment",
+    download_url="https://github.com/schanzel/powerline-ifinfo-segment/tarball/v0.1.0",
     install_requires=["ifcfg"],
     python_requires="~=3.8",
 )
