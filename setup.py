bf5c14d840b232851d722f8cde01036ca1ff6e69
Benjamin Schanzel <benjamin.schanzel@bmw.de>
2021-09-09 15:39:05 +0200

    update setup.py

--- setup.py
+++ setup.py
@@ -1,14 +1,24 @@
 from setuptools import setup
 
+with open("README.md", "r", encoding="utf-8") as fh:
+    long_description = fh.read()
+
 setup(
     name="powerline-ifinfo-segment",
-    version="0.1.0",
+    version="1.0.0",
     description="Network Inteface Information Segment for Powerline",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
     author="Benjamin Schanzel",
     author_email="b.schanzel@gmail.com",
     packages=["powerline_ifinfo"],
     url="https://github.com/schanzel/powerline-ifinfo-segment",
-    download_url="https://github.com/schanzel/powerline-ifinfo-segment/tarball/v0.1.0",
+    download_url="https://github.com/schanzel/powerline-ifinfo-segment/tarball/v1.0.0",
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
     install_requires=["ifcfg"],
     python_requires="~=3.8",
 )
